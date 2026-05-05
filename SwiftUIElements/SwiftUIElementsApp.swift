98f3322901854b20d06f87acf587788d0fec2e43
Chetan Purohit <41613931+Chetan81289@users.noreply.github.com>
2026-05-05 11:43:21 +0530

    Initial commit

---
+++ SwiftUIElements/SwiftUIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIElementsApp.swift
+//  SwiftUIElements
+//
+//  Created by Chetan purohit on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
